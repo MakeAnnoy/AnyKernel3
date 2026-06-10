7c5a2bacd0e1b6252d77a055dc0493400dbb6264
Lolipop <makeanonim@gmail.com>
2026-06-11 01:31:55 +0700

    Update kernel string for Xiaomi Redmi 4X

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # global properties
 script="$0"
 properties() { '
-kernel.string=perf⁺ kernel redmi 4x a.k.a santoni
+kernel.string=kernel for Xiaomi Redmi 4X a.k.a santoni
 do.devicecheck=1
 do.modules=0
 do.systemless=1
